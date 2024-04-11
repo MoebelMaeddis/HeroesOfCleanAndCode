dd7020dd5062596fff00d7c6aa931df8fa4f6949
YannickYil001 <105918270+YannickYil001@users.noreply.github.com>
2024-04-11 12:04:36 +0200

    added Xml test

--- HeroesOfCleanAndCode/Test_Project/XmlReader.cs
+++ HeroesOfCleanAndCode/Test_Project/XmlReader.cs
@@ -5,16 +5,31 @@ using System.Xml;
 
 namespace HeroesOfCleanAndCode
 {
-    internal class XmlReader
+    public class XmlReader
     {
         private string url { get; }
-        private XmlDocument xmlDocument { get; }
+        public XmlDocument xmlDocument { get; }
 
-        public XmlReader(string url)
+        /*public XmlReader(string url)
         {
             this.url = url;
             xmlDocument = new XmlDocument();
             xmlDocument.Load(this.url);
+        }*/
+
+        public interface IXmlLoader
+        {
+            string LoadXmlFromUrl(string url);
         }
+
+        public XmlReader(string url, IXmlLoader xmlLoader)
+        {
+            this.url = url;
+            xmlDocument = new XmlDocument();
+            string xmlContent = xmlLoader.LoadXmlFromUrl(url);
+            xmlDocument.LoadXml(xmlContent);
+        }
+        
+
     }
 }
---
+++ HeroesOfCleanAndCode/xUnit.Tests/XmlReaderTest.cs
@@ -0,0 +1,36 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HeroesOfCleanAndCode;
+using XmlReader = HeroesOfCleanAndCode.XmlReader;
+
+namespace xUnit.Tests
+{
+    public class XmlReaderTest
+    {
+        [Fact]
+        public void XmlReader_Loads_Url_Content()
+        {
+            // Arrange
+            string fakeUrl = "http://fake.url";
+            string fakeXmlContent = "<root><element>test</element></root>";
+
+            var fakeXmlLoader = A.Fake<XmlReader.IXmlLoader>();
+            A.CallTo(() => fakeXmlLoader.LoadXmlFromUrl(A<string>.Ignored))
+                .Returns(fakeXmlContent);
+
+            // Act
+            var xmlReader = new XmlReader(fakeUrl, fakeXmlLoader);
+
+            // Assert
+            Assert.Equal(fakeXmlContent, xmlReader.xmlDocument.OuterXml);
+            
+            xmlReader.xmlDocument.OuterXml.Should().BeEquivalentTo(fakeXmlContent);
+        }
+
+    }
+}
